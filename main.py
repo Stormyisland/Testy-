e9e008d63cf15ab810421b4411c87eea04b3b2c9
Stormyisland <jacqueslavoie2015@gmail.com>
2024-06-08 01:45:44 -0700

    commit message 2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+with open( "test.txt", "w") as file:
+  file.write("3 love\n 6 computers\n 2 dogs\n 4 cats\n 1,I\n 5, you\n 7,bird\n 8, me")
 
-
-file = open( "codelist.txt", "w")
-file.write("3 love\n 6 computers\n 2,dogs\n 4 cats\n  1,I\n 5, you\n 7,bird\n")
-file.close()
+def make_list():
+    final_list  = []
+    file = final_list 
+    print(list)
+    
